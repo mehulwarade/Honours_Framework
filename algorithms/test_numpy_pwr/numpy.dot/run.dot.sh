16f7666b3f77e344671db86eea9cb4454ec64c1b
Mehul RPi netboot <mehulwarade@gmail.com>
2020-08-01 16:57:43 +1000

    final edit done before testing

--- algorithms/test_numpy_pwr/numpy.dot/run.dot.sh
+++ algorithms/test_numpy_pwr/numpy.dot/run.dot.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-for SIZE in 36 60 120 144; do
+for SIZE in 36; do
+# for SIZE in 600 720 840 960; do
+    echo SIZE: $SIZE
     echo SIZE: $SIZE >> results/result.log
-    for TEST in 1 2 3 4 5 6 7 8 9 10; do
+    date '+%Y%m%d%H%M%S' >> results/result.log
+    echo ' ' >> results/result.log
+    
+    for TEST in 1 2 3; do
+    # for TEST in 1 2 3 4 5 6 7 8 9 10; do    
+        echo Test Number: $TEST
+        sleep 5
+        echo Starting
         python3 dot.py $SIZE >> results/result.log
-        sleep 0.5
+        echo End
+        sleep 5
+    
     done
-
+    echo ' ' >> results/result.log
+    date '+%Y%m%d%H%M%S' >> results/result.log
     echo ' ' >> results/result.log
 done
 
