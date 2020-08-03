5356aadfc7f2d29ac69f4b6c826ea1b459da4729
Mehul Warade <mehulwarade@gmail.com>
2020-08-03 16:32:45 +1000

    updating the comments

--- switch_connect/index.js
+++ switch_connect/index.js
@@ -42,6 +42,27 @@ mysql-> FLUSH PRIVILEGES;
 https://stackoverflow.com/a/51918364
 
 
+mysql terminal commands:
+
+//To show the output without the fancy lining:
+https://stackoverflow.com/a/9558954 
+
+//All mysql queries:
+https://gist.github.com/hofmannsven/9164408
+
+//Some mysql command (terminal)
+mysql -u [USER] -p [DATABASE_NAME] -ss -e "select * from [TABLE_NAME]"
+mysql -u [USER] -p [DATABASE_NAME] -ss -e "select [COLUMN_NAME] from [TABLE_NAME] where [COLUMN_NAME] between [VALUES] and [VALUES]"
+mysql -u [USER] -p [DATABASE_NAME] -e "select * from [TABLE_NAME] where [COLUMN_NAME] between [VALUES] and [VALUES]"
+mysql -u [USER] -p [DATABASE_NAME] -e "select * from [TABLE_NAME]"
+
+mysql -u root -p mytestdb -ss -e "select * from test_single_power"
+mysql -u root -p mytestdb -ss -e "select p1 from test_single_power where timestamp between 1596269829953 and 1596270105047"
+mysql -u root -p mytestdb -e "select * from test_single_power where timestamp between 1596269240632 and 1596269402026"
+mysql -u root -p mytestdb -e "select * from test_single_power"
+
+
+
 */
 let mysql = require('mysql');
 let sql = mysql.createConnection({
