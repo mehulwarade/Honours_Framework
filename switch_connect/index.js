76f00e139d31ecdd6592ea947ef9dd056cf533cf
Mehul RPi netboot <mehulwarade@gmail.com>
2020-08-03 16:32:03 +1000

    getting table value from env

--- switch_connect/index.js
+++ switch_connect/index.js
@@ -44,7 +44,28 @@ mysql-> FLUSH PRIVILEGES;
 https://stackoverflow.com/a/51918364
 
 
+mysql terminal commands:
+
+//To show the output without the fancy lining:
+https://stackoverflow.com/a/9558954 
+
+//All mysql queries:
+https://gist.github.com/hofmannsven/9164408
+
+//Some mysql command (terminal)
+mysql -u [USER] -p [DATABASE_NAME] -ss -e "select * from [TABLE_NAME]"
+mysql -u [USER] -p [DATABASE_NAME] -ss -e "select [COLUMN_NAME] from [TABLE_NAME] where [COLUMN_NAME] between [VALUES] and [VALUES]"
+mysql -u [USER] -p [DATABASE_NAME] -e "select * from [TABLE_NAME] where [COLUMN_NAME] between [VALUES] and [VALUES]"
+mysql -u [USER] -p [DATABASE_NAME] -e "select * from [TABLE_NAME]"
+
+mysql -u root -p mytestdb -ss -e "select * from test_single_power"
+mysql -u root -p mytestdb -ss -e "select p1 from test_single_power where timestamp between 1596269829953 and 1596270105047"
+mysql -u root -p mytestdb -e "select * from test_single_power where timestamp between 1596269240632 and 1596269402026"
+mysql -u root -p mytestdb -e "select * from test_single_power"
+
+
 */
+
 let mysql = require('mysql');
 let sql = mysql.createConnection({
   host     : 'localhost',
@@ -71,7 +92,7 @@ sql.query("CREATE DATABASE if not exists" + process.env.MYSQL_DATABASE_NAME, fun
 changeDB();
 
 //Check or create table
-var q = "CREATE TABLE if not exists power (timestamp BIGINT(255), description VARCHAR(255), p1 FLOAT(12), p2 FLOAT(12), p3 FLOAT(12), p4 FLOAT(12), p5 FLOAT(12), p6 FLOAT(12), p7 FLOAT(12))";
+var q = "CREATE TABLE if not exists " + process.env.MYSQL_TABLE_NAME + " (timestamp BIGINT(255), description VARCHAR(255), p1 FLOAT(12), p2 FLOAT(12), p3 FLOAT(12), p4 FLOAT(12), p5 FLOAT(12), p6 FLOAT(12), p7 FLOAT(12))";
 sql.query(q, function (err, result) {
   if (err){
     console.log('Error or table already exists: Power. Continuing...');
@@ -101,7 +122,7 @@ telcon.connect(params)
           pwr[7] = parseFloat(res.slice(1529,1589).slice(15,18))
           // console.log(pwr);   
           console.log('Updating data...')
-          var qinsertpwr = "INSERT INTO power (timestamp, description, p1, p2, p3, p4, p5, p6, p7) VALUES ("+ Date.now()+","+"' '"+","+pwr[1]+","+pwr[2]+","+pwr[3]+","+pwr[4]+","+pwr[5]+","+pwr[6]+","+pwr[7]+")";
+          var qinsertpwr = "INSERT INTO " + process.env.MYSQL_TABLE_NAME + " (timestamp, description, p1, p2, p3, p4, p5, p6, p7) VALUES ("+ Date.now()+","+"' '"+","+pwr[1]+","+pwr[2]+","+pwr[3]+","+pwr[4]+","+pwr[5]+","+pwr[6]+","+pwr[7]+")";
           // console.log(qinsertpwr);
 
           sql.query(qinsertpwr, function (err, result) {
