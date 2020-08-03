37e7b2e069651689229520244ac636d279dca36e
Mehul RPi netboot <mehulwarade@gmail.com>
2020-08-03 16:20:23 +1000

    edited to compute on large arrays

--- algorithms/test_numpy_pwr/numpy.dot/dot.py
+++ algorithms/test_numpy_pwr/numpy.dot/dot.py
@@ -5,10 +5,11 @@ from time import time
 import sys
 
 size = int( sys.argv[1])
-
-A = np.loadtxt('../arrays/array1/%s.array' %size)
-B = np.loadtxt('../arrays/array2/%s.array' %size)
-
+print('Loading Array1 into memory')
+A = np.loadtxt('../arrays/array1/%s.array' %size, dtype='uint8')
+print('Loading Array2 into memory')
+B = np.loadtxt('../arrays/array2/%s.array' %size, dtype='uint8')
+print('Calculating...')
 t = time()
 np.dot(A, B)
 # print(np.dot(A,B)) #python3 dot.py 120 ####check result
--- algorithms/test_numpy_pwr/numpy.dot/run.dot.sh
+++ algorithms/test_numpy_pwr/numpy.dot/run.dot.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-for SIZE in 36; do
-# for SIZE in 600 720 840 960; do
+for SIZE in 7080; do
     echo SIZE: $SIZE
     echo SIZE: $SIZE >> results/result.log
+
+    #https://stackoverflow.com/a/16548827
+
     echo $(($(date +%s%N)/1000000)) >> results/result.log
     echo ' ' >> results/result.log
+
+    sleep 5
+    echo Starting
+    python3 dot.py $SIZE >> results/result.log
+    echo End
+    sleep 5
     
-    for TEST in 1 2 3; do
-    # for TEST in 1 2 3 4 5 6 7 8 9 10; do    
-        echo Test Number: $TEST
-        sleep 5
-        echo Starting
-        python3 dot.py $SIZE >> results/result.log
-        echo End
-        sleep 5
-    
-    done
     echo ' ' >> results/result.log
     echo $(($(date +%s%N)/1000000)) >> results/result.log
     echo ' ' >> results/result.log
