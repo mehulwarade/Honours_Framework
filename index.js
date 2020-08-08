8c273430779fe3cd302213460e77f082712b2de3
Mehul Warade <mehulwarade@gmail.com>
2020-08-08 10:47:57 +1000

    config in - no config module needed

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const config = require('config');
+const config = require('./configuration.json');
+console.log(`Application port: ${config.app.port}`);
+// let appPort = config.get('app.port');
+// console.log(`Application port: ${appPort}`);
 
-let appPort = config.get('app.port');
-console.log(`Application port: ${appPort}`);
+// let dbHost = config.get('db.host');
+// console.log(`Database host: ${dbHost}`);
 
-let dbHost = config.get('db.host');
-console.log(`Database host: ${dbHost}`);
+// let dbPort = config.get('db.port');
+// console.log(`Database port: ${dbPort}`);
 
-let dbPort = config.get('db.port');
-console.log(`Database port: ${dbPort}`);
+// let dbName = config.get('db.name');
+// console.log(`Database name: ${dbName}`);
 
-let dbName = config.get('db.name');
-console.log(`Database name: ${dbName}`);
-
-console.log('NODE_ENV: ' + config.util.getEnv('NODE_ENV'));
+// console.log('NODE_ENV: ' + config.util.getEnv('NODE_ENV'));
