43fecc64b8f49e830657fdcb902c6434197b3c4b
Mehul Warade <mehulwarade@gmail.com>
2020-08-09 14:35:17 +1000

    first batch of cluster experiments done

--- switch_connect/mysql_output.sh
+++ switch_connect/mysql_output.sh
@@ -3,4 +3,4 @@ echo Table_Name: $1
 echo Start_Time: $2
 echo End_Time: $3
 
-mysql -u root -p mytestdb -ss -e "select p1 from "$1" where timestamp between "$2" and "$3
+mysql -u root -p mytestdb -ss -e "select p1 from "$1" where timestamp between "$2" and "$3 >> $1.log
