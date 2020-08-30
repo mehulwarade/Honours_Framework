523fcc17e62b2e780ee34491e040ac807dfcbce4
Mehul Warade <mehulwarade@gmail.com>
2020-08-30 15:55:17 +1000

    readme update

--- README.md
+++ README.md
@@ -25,4 +25,15 @@ Need MySQL server installed on local machine
 sudo apt-get update
 <!-- sudo apt install php -->
 sudo apt install mariadb-server  => sudo mysql_secure_installation
-sudo apt install npm
+sudo apt install npm (sudo apt install nodejs npm => node -v)
+
+
+# Running from scratch:
+### RPi netboot. (location: /nfs/client/home/pi/** <= so that its accessible to other nodes =>)
+`sudo apt-get update`
+
+`sudo apt install npm`
+    `=> node -v`
+
+`sudo apt install mariadb-server`
+    `=> sudo mysql_secure_installation`
---
+++ tests/nodejs_child_process/index.js
@@ -0,0 +1,26 @@
+const { spawn } = require('child_process');
+// const { exec } = require('child_process');
+var mp='mpi'
+const ls = spawn('ssh', ['pimaster', `cd ${mp} && mpirun -np 4 python3 helloworld.py`]);
+// const lsa = spawn('ls', ['-lh', '/usr']);
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
+// exec('ssh pimaster touch ~/abc', (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(`exec error: ${error}`);
+//       return;
+//     }
+//     console.log(`stdout: ${stdout}`);
+//     console.error(`stderr: ${stderr}`);
+//   });
